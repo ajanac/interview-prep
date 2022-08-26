793885a0f5dce46e99b934ce4ee0126532fb633b
ajanac <ajanacs@gmail.com>
2022-08-26 06:56:12 -0500

    fix(aug: 25): added the optimized solution

--- src/main/scala/august/CountElems.scala
+++ src/main/scala/august/CountElems.scala
@@ -8,7 +8,6 @@ package august
  *          *******************************
  *          Counting elements in two arrays
  *          *******************************
-            EasyAccuracy: 47.9%Submissions: 46724Points: 2
             Given two unsorted arrays arr1[] and arr2[].
             They may contain duplicates.
             For each element in arr1[] count elements less than or equal to it in array arr2[].
@@ -62,12 +61,95 @@ object CountElemsNaiveSolution
       output
     }
 }
+
+/**
+ *  Algorithm:
+    *************
+    Sort the second array.
+    Traverse through the elements of the first array from start to end.
+    For every element in the first array.
+    Do a binary search on the second array and find the index of the largest element smaller than or equal to the element of the first array.
+    The index of the largest element will give the count of elements. Print the count for every index.
+    *******************
+    Complexity Analysis:
+    ********************
+    Time Complexity: O(mlogn + nlogn).
+    Considering arr1[] and arr2[] of sizes m and n respectively.
+    Space Complexity: O(1).
+    As no extra space is required
+ */
+object CountElemsEfficientSolution
+{
+  /**
+   * @param arr
+   * @param l
+   * @param h
+   * @param elem
+   * @return   the index
+               of largest element
+               smaller than equal to 'x'
+               in 'arr'. For duplicates
+               it returns the last index
+               of occurrence of required
+               element. If no such element
+               exits then it returns -1.
+   */
+  private def binarySearch(arr: Array[Int], l: Int, h: Int, elem: Int): Int =
+  {
+    var low  = l
+    var high = h
+    while(low <= high)
+      {
+        val mid = (low + high) / 2
+        if (arr(mid) <= elem)
+          low = mid + 1
+        else
+          high = mid - 1
+      }
+      high
+  }
+
+  /**
+   * @param arrOne
+   * @param arrTwo
+   * @param m
+   * @param n
+   */
+  def countElemLessThanOrEqual(arrOne: Array[Int], arrTwo: Array[Int], m: Int, n: Int): Unit =
+    {
+      // sort the second array
+      val sortedArrTwo = arrTwo.sorted
+      // Traverse through the elements of the first array from start to end.
+      Range(0, m).foreach{i =>
+        // Do a binary search on the second array and
+        // find the index of the largest element smaller than or
+        // equal to the element of the first array.
+        val index = binarySearch(sortedArrTwo, 0, n - 1, arrOne(i))
+        // The index of the largest element will give the count
+        // of elements. Print the count for every index.
+        print(s"${index + 1} ")
+      }
+    }
+}
+
 object CountElems extends App
 {
   val arr1   = Array(4,8,7,5,1)
   val arr2   = Array(4,48,3,0,1,1,5)
+  //output: 5 6 6 6 3
+  val anotherArr1 = Array(1, 2, 3, 4, 7, 9)
+  val anotherArr2 = Array(0, 1, 2, 1, 1, 4 )
+  //output: 4 5 5 6 6 6
+
+  println("*************")
   val output = CountElemsNaiveSolution.countLessThanEqualTo(arr1, arr2)
   println(output.mkString(" "))
-  //Output: 5 6 6 6 3
-  // Will come back tomorrow to implement the efficient solution
+  CountElemsEfficientSolution.countElemLessThanOrEqual(arr1, arr2, arr1.length, arr2.length)
+  println
+  println("*************")
+  val anotherOutput = CountElemsNaiveSolution.countLessThanEqualTo(anotherArr1, anotherArr2)
+  println(anotherOutput.mkString(" "))
+  CountElemsEfficientSolution.countElemLessThanOrEqual(anotherArr1, anotherArr2, anotherArr1.length, anotherArr2.length)
+  println()
+  println("*************")
 }
