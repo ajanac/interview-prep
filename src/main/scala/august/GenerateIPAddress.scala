aed47050b08259b02adf5acec59869db951ebb59
ajanac <ajanacs@gmail.com>
2022-08-29 08:06:04 -0500

    fix(august 25): started implementing optimized solution using DP

--- src/main/scala/august/GenerateIPAddress.scala
+++ src/main/scala/august/GenerateIPAddress.scala
@@ -1,6 +1,6 @@
 package august
 
-import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.{ArrayBuffer, ListBuffer}
 
 /**
  * @author : ajanasathian
@@ -110,6 +110,98 @@ object GenerateIPAddressNaiveSolution
   }
 }
 
+/**
+ *          Efficient Solution:
+ *          *******************
+ *          Another Efficient Approach (Dynamic Programming): There is a dp approach exist for this problem and
+ *          can be solved in time complexity O(n*4*3)=O(12n)=O(n) and space complexity O(4n).
+ *          Approach: We know that there are only 4 parts of the IP. We start iterating from the end of string
+ *          and goes to the start of string. We create a dp 2D-array of size (4 x N). There can be only 2 values
+ *          in the dp array i.e. 1(true) or 0(false). dp[0][i] tells if we can create 1 part of IP from the substring
+ *          starting from i to end of string. Similarly, dp[1][i] tells if we can create 2 parts of IP from the
+ *          substring starting from i to end of string. After creating the dp array, we start creating the valid IP addresses.
+ *          We start from the bottom left corner of the 2D dp array.
+ *          We only iterate 12 times(worst case) but those also will be the valid IP addresses because we only form valid IP addresses.
+ */
+import scala.util.control.Breaks._
+
+object GenerateIPAddressEfficientSolution
+{
+  val list = scala.collection.mutable.ArrayBuffer[String]()
+
+  def restoreIpAddresses(s: String): Array[String] =
+    {
+      val n = s.length
+      if (n < 4 || n > 12) return list.toArray
+
+      val dp = Array.ofDim[Int](4, n)
+      Range(0, 4).foreach{i =>
+        breakable{
+          Range(n - 1, 0, -1).foreach{j =>
+            if (i == 0)
+            {
+              val sub = s.substring(j)
+              if (isValid(sub))
+                dp(i)(j) = 1
+              else if (j < n - 3)
+                break()
+            }
+            else
+              {
+                if (j <= n - i)
+                {
+                  (1 to 3).filter(j + _ <= n).foreach{k =>
+                    val temp = s.substring(j, j + k)
+                    if (isValid(temp))
+                      {
+                        if (j + k < n && dp(i - 1)(j + k) == 1)
+                          {
+                            dp(i)(j) = 1
+                            break()
+                          }
+                      }
+                  }
+                }
+              }
+          }
+        }
+      }
+      if (dp(3)(0) == 0)
+        return list.toArray
+      createIpFromDP(dp, 3, 0, s, "")
+      list.toArray
+    }
+
+  private def createIpFromDP(dp: Array[Array[Int]], r: Int, c: Int, s: String, ans: String): Unit =
+    {
+      var ansOne = ans
+      if (r == 0)
+        {
+          ansOne += s.substring(c)
+          list.append(ans)
+          return
+        }
+      (1 to 3).filter(c + _ < s.length).foreach{i =>
+        if (isValid(s.substring(c, c + i)) && dp(r - 1)(0) == 1)
+          {
+            createIpFromDP(dp, r - 1, c + i, s, ans + s.substring(c, c + i) + ".")
+          }
+      }
+    }
+
+  private def isValid(ip: String): Boolean =
+    {
+      val a = ip.split("[.]")
+      a.foreach{s =>
+        val i = s.toInt
+        if (s.length > 3 || i < 0 || i > 255) return false
+        if (s.length > 1 && i == 0) return  false
+        if (s.length > 1 && i != 0 && s.charAt(0) == '0') return false
+      }
+      true
+    }
+}
+
 object GenerateIPAddress extends App
 {
   val inputStr = "11211"
@@ -120,5 +212,12 @@ object GenerateIPAddress extends App
   1.1.21.1
   1.12.1.1
   11.2.1.1*/
-  // Need to come back for the optimized solution DP
+  println("***********")
+  val output = GenerateIPAddressEfficientSolution.restoreIpAddresses("25525511135")
+  // output is empty need to come back for the dp solution implementation.
+  /**output
+   * [255.255.11.135, 255.255.111.35]
+   */
 }
+
+
