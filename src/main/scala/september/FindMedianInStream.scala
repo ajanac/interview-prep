7cdaead72cb7891786cff2d179b15b0dedcf979f
ajanac <ajanacs@gmail.com>
2022-09-05 16:21:09 -0500

    fix(septemeber5): problem of the day

--- src/main/scala/september/FindMedianInStream.scala
+++ src/main/scala/september/FindMedianInStream.scala
@@ -50,33 +50,52 @@ package september
             Constraints:
             1 <= N <= 106
             1 <= x <= 106
+            https://www.youtube.com/watch?v=VmogG01IjYc
  * */
 import scala.collection.mutable
 
 object FindMedianInStreamSolution
 {
-  val left = mutable.PriorityQueue[Int]()(Ordering[Int].reverse)
-  val right = mutable.PriorityQueue[Int]()
+  val minHeap = mutable.PriorityQueue[Int]()(Ordering[Int].reverse)
+  val maxHeap = mutable.PriorityQueue[Int]()
 
-  def insertHeap(x: Int): Unit =
+  def processTime(): Unit =
+    {
+      val n = scala.io.StdIn.readInt()
+      Range(0, n).foreach{i =>
+        val x = scala.io.StdIn.readInt()
+        if (maxHeap.isEmpty || x < maxHeap.head)
+          maxHeap.addOne(x)
+        else
+          minHeap.addOne(x)
+        balanceHeaps()
+        println(s"Median so far: ${getMedian}")
+      }
+
+    }
+
+  def getMedian: Double =
   {
-    if (right.size > 0 && x >= right.head) right.addOne(x)
-    else left.addOne(x)
-    balanceHeaps()
+    val s1 = minHeap.size
+    val s2 = maxHeap.size
+
+    if (s1 == s2) (minHeap.head.toDouble + maxHeap.head) / 2
+    else
+    {
+      if (s1 > s2)
+        minHeap.head
+      else
+        maxHeap.head
+    }
   }
 
-  private def balanceHeaps(): Unit =
-      if (left.size - right.size == 2) right.addOne(left.dequeue)
-      else if (right.size - left.size == 2) left.addOne(right.dequeue)
-
-  def getMedian(): Double =
-    {
-      var median: Double = 0
-      if(left.size > right.size) median =(left.head).toDouble
-      else if(left.size == right.size) median = ((left.head + right.head)/2).toDouble
-      else median = (right.head).toDouble
-      median
-    }
+  def balanceHeaps(): Unit =
+  {
+    if (maxHeap.size > (minHeap.size + 1))
+      minHeap.addOne(maxHeap.dequeue())
+    else if (minHeap.size > (maxHeap.size + 1))
+      maxHeap.addOne(minHeap.dequeue())
+  }
 }
 
 object FindMedianInStream extends App
@@ -87,9 +106,5 @@ object FindMedianInStream extends App
   val N_2 = 3
   val X_2 = Array(5, 10, 15)
   // Output: 5 7.5 10
-  (0 until N_2).foreach{i =>
-    FindMedianInStreamSolution.insertHeap(X_2(i))
-    println(FindMedianInStreamSolution.getMedian())
-  }
-  //Need to comeback output is not correct
+  FindMedianInStreamSolution.processTime()
 }
